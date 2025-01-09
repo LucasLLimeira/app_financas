bbc6733062e31952983de47360cb9b7510eb771b
Lucas De Lucena Limeira <lucasdllimeira@hotmail.com>
2025-01-09 20:14:31 -0300

    ultimo update de hoje

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,28 +7,32 @@ import Entries from './routes/Entries';
 import Exits from './routes/Exit';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Main from './components/main';
+import GlobalProvider from './context/GlobalContext';
 
 function App() {
   return (
-    <Router>
-      <div className="relative min-h-screen flex">
-        {/* Sidebar */}
-        <Sidebar />
+    <GlobalProvider>
+      <Router>
+        <div className="relative min-h-screen flex">
+          {/* Sidebar */}
+          <Sidebar />
 
-        {/* Área principal */}
-        <div className="flex-1">
-          <BackgroundTrailAnimation />
-          <Main>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/transactions" element={<Transactions />} />
-              <Route path="/entries" element={<Entries />} />
-              <Route path="/exits" element={<Exits />} />
-            </Routes>
-          </Main>
+          {/* Área principal */}
+          <div className="flex-1">
+            <BackgroundTrailAnimation />
+            <Main>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/transactions" element={<Transactions />} />
+                <Route path="/entries" element={<Entries />} />
+                <Route path="/exits" element={<Exits />} />
+              </Routes>
+            </Main>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </GlobalProvider>
+    
   );
 }
 
